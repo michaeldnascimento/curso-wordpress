391bfa7569c1da806f6aac0ea41c2c9cd748689c
Michael D. Nascimento <michaeldnascimento@hotmail.com>
2022-03-02 10:09:35 -0300

    Didebars ou widgets - seção 3

--- www/wp-content/themes/curso/functions.php
+++ www/wp-content/themes/curso/functions.php
@@ -1,22 +1,33 @@
-<?php 
+<?php
 
 //Carregando nossos scripts e folhas de estilos
 function load_scripts(){
-    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array(), '5.1.0', true );
-    wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.0', 'all' );
-    //wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
-    //wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0', 'all' );
-	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '0.1', 'all' );
+    //wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array(), '5.1.0', true );
+    //wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.0', 'all' );
+    wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
+    wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0', 'all' );
+    wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
-//Registrando nossos menus
 
-register_nav_menus(
-    array(
-        'my_main_menu' => 'Main Menu',
-        'my_footer_menu' => 'Menu Footer'
-    )
-);
+// Função de Configuração do Tema
+function wpcurso_config(){
+    // Registrando nossos menus
+    register_nav_menus(
+        array(
+            'my_main_menu' => 'Main Menu',
+            'footer_menu' => 'Footer Menu'
+        )
+    );
 
+    $args = array(
+        'height'	=> 225,
+        'width'		=> 1920
+    );
+    add_theme_support( 'custom-header', $args );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-formats', array( 'video', 'image' ) );
+}
+add_action( 'after_setup_theme', 'wpcurso_config', 0 );
 
--- www/wp-content/themes/curso/index.php
+++ www/wp-content/themes/curso/index.php
@@ -1,15 +1,52 @@
-<!-- call header -->
-<?php get_header() ?>
-<div class="content-area">
-    <main>
-        <section class="slide">Slider</section>
-        <section class="services">Serviços</section>
-        <section class="middle-area">
-            <aside class="sidebar">Barra Lateral</aside>
-            <div class="news">Notícias</div>
-        </section>
-        <section class="map">Mapa</section>
-    </main>
-</div>
-<?php get_footer() ?>
-<!-- call footer -->
+<?php get_header(); ?>
+
+    <img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
+    <div class="content-area">
+        <main>
+            <section class="slide">
+                <div class="container">
+                    <div class="row">Slide</div>
+                </div>
+            </section>
+            <section class="services">
+                <div class="container">
+                    <div class="row">Serviços</div>
+                </div>
+            </section>
+            <section class="middle-area">
+                <div class="container">
+                    <div class="row">
+                        <aside class="sidebar col-md-4">Barra Lateral</aside>
+                        <div class="news col-md-8">
+                            <?php
+                            // Se houver algum post
+                            if( have_posts() ):
+                                // Enquanto houver posts, mostre-os pra gente
+                                while( have_posts() ): the_post();
+
+                                    ?>
+
+                                    <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+
+                                <?php
+                                endwhile;
+                            else:
+                                ?>
+
+                                <p>There's nothing yet to be displayed...</p>
+
+                            <?php endif; ?>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="map">
+                <div class="container">
+                    <div class="row">Mapa</div>
+                </div>
+            </section>
+        </main>
+    </div>
+<?php get_footer(); ?>
